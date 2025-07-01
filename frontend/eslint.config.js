2f49dda8fce71875507c26f037b2917600630f63
workspace014-admin <oscar.osorio1884@gmail.com>
2025-07-01 07:42:18 -0500

    refactor: update ESLint configuration for improved code style and formatting

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,9 +1,9 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -14,10 +14,31 @@ export default tseslint.config([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      'plugin:prettier/recommended',
     ],
+    plugins: ['prettier'],
+    rules: {
+      // reglas personalizadas
+      'prettier/prettier': [
+        'error',
+        {
+          semi: true,
+          singleQuote: true,
+          printWidth: 100,
+          tabWidth: 2,
+          trailingComma: 'all',
+          arrowParens: 'always',
+        },
+      ],
+      indent: 'off',
+      '@typescript-eslint/indent': 'off',
+      'linebreak-style': 'off',
+      quotes: 'off',
+      semi: 'off', 
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
   },
-])
+]);
